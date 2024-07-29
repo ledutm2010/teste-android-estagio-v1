99cd95335623e89b5ac8ec829cbb361636d40c32
ledutm2010 <45878959+ledutm2010@users.noreply.github.com>
2024-07-29 17:59:48 -0300

    Revert "realizando o desafio"
    This reverts commit ea737902d6f5802b361c73ddf741030cf0f01a06.

--- app/src/main/java/com/example/olhovivo/model/Constantes.java
+++
@@ -1,9 +0,0 @@
-package com.example.olhovivo.model;
-
-import com.google.gson.annotations.SerializedName;
-
-public class Constantes {
-    public static final String OLHOVIVO_URL = "https://api.olhovivo.sptrans.com.br/v2.1/";
-    @SerializedName("token")
-    public static final String OLHOVIVO_TOKEN = "a27e528ba4b915f0cd7543874868b976bafbccf4d4370c87863925dfd4690be7";
-}
